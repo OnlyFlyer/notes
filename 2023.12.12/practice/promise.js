89b7b5fe9e9cbc373e474fe6bde99c023be76289
FlyerWu <scofield90.email@gmail.com>
2024-01-05 15:08:54 +0800

    update

--- 2023.12.12/practice/promise.js
+++ 2023.12.12/practice/promise.js
@@ -1,89 +1,90 @@
-const PromisePending = 'pending';
-const PromiseFulfilled = 'fulfilled';
-const PromiseRejected = 'rejected';
 
-// class MyPromiseClass {
-//   promiseStatus;
-//   promiseResult;
-//   promiseFulfilledCallback;
-//   promiseRejectedCallback;
-//   constructor(executor) {
-//     this.bindInitialValue();
-//     try {
-//       executor(this.resolve, this.reject);
-//     } catch (err) {
-//       this.reject(err);
-//       throw new Error(err);
-//     }
-//   }
-//   bindInitialValue () {
-//     this.promiseStatus = PromisePending;
-//     this.promiseResult = null;
-//     this.promiseFulfilledCallback = [];
-//     this.promiseRejectedCallback = [];
-//   };
-//   resolve = (result) => {
-//     if (this.promiseStatus !== PromisePending) return;
-//     this.promiseStatus = PromiseFulfilled;
-//     this.promiseResult = result;
-//     while (this.promiseFulfilledCallback.length) {
-//       const cb = this.promiseFulfilledCallback.shift();
-//       cb(this.promiseResult);
-//     }
-//   };
-//   reject = (reason) => {
-//     if (this.promiseStatus !== PromisePending) return;
-//     this.promiseStatus = PromiseRejected;
-//     this.promiseResult = reason;
-//     while (this.promiseRejectedCallback.length) {
-//       const cb = this.promiseRejectedCallback.shift();
-//       cb(this.promiseResult);
-//     }
-//   };
+class MyPromise {
+  static PENDING = 'pending';
+  static FULFILLED = 'fulfilled';
+  static REJECTED = 'rejected';
+  promiseStatus;
+  promiseResult;
+  promiseFulfilledCallback;
+  promiseRejectedCallback;
+  constructor(executor) {
 
-//   then = (onFulfilled, onRejected) => {
-//     onFulfilled = typeof onFulfilled === 'function'
-//       ? onFulfilled
-//       : (val) => val;
-//     onRejected = typeof onRejected === 'function'
-//       ? onRejected
-//       : (err) => { throw new Error(err) };
-//     return new MyPromiseClass((resolve, reject) => {
-//       const resolvePromise = (cb) => {
-//         try {
-//           const res = cb(this.promiseResult);
-//           if (res instanceof MyPromiseClass) {
-//             res.then(resolve, reject);
-//           } else {
-//             resolve(res);
-//           }
-//         } catch (err) {
-//           reject(err);
-//         }
-//       };
-//       if (this.promiseStatus === PromiseFulfilled) {
-//         resolvePromise(onFulfilled);
-//         // onFulfilled(this.promiseResult);
-//       } else if (this.promiseStatus === PromiseRejected) {
-//         resolvePromise(onRejected);
-//         // onRejected(this.promiseResult);
-//       } else if (this.promiseStatus === PromisePending) {
-//         this.promiseFulfilledCallback.push(resolvePromise.bind(this, onFulfilled));
-//         this.promiseRejectedCallback.push(resolvePromise.bind(this, onRejected));
-//       };
-//     });
-//   };
-// };
+    this.bindInitialValue();
+    try {
+      executor(this.resolve, this.reject);
+    } catch (err) {
+      this.reject(err);
+      throw new Error(err);
+    }
+  }
+  bindInitialValue () {
+    this.promiseStatus = MyPromise.PENDING;
+    this.promiseResult = null;
+    this.promiseFulfilledCallback = [];
+    this.promiseRejectedCallback = [];
+  };
+  resolve = (result) => {
+    if (this.promiseStatus !== MyPromise.PENDING) return;
+    this.promiseStatus = MyPromise.FULFILLED;
+    this.promiseResult = result;
+    while (this.promiseFulfilledCallback.length) {
+      const cb = this.promiseFulfilledCallback.shift();
+      cb(this.promiseResult);
+    }
+  };
+  reject = (reason) => {
+    if (this.promiseStatus !== MyPromise.PENDING) return;
+    this.promiseStatus = MyPromise.REJECTED;
+    this.promiseResult = reason;
+    while (this.promiseRejectedCallback.length) {
+      const cb = this.promiseRejectedCallback.shift();
+      cb(this.promiseResult);
+    }
+  };
 
-// var a = new MyPromiseClass((resolve, reject) => {
-//   console.log(1);
-//   setTimeout(() => {
-//     resolve(1);
-//   }, 2000);
-// }).then(res => {
-//   console.log(res, '--第一个then');
-//   return res + 1;
-// }, err => err).then((res) => { console.log(res, '--第二个then'); }, (err) => { console.log(err, '--失败'); });;
+  then = (onFulfilled, onRejected) => {
+    onFulfilled = typeof onFulfilled === 'function'
+      ? onFulfilled
+      : (val) => val;
+    onRejected = typeof onRejected === 'function'
+      ? onRejected
+      : (err) => { throw new Error(err) };
+    return new MyPromise((resolve, reject) => {
+      const resolvePromise = (cb) => {
+        try {
+          const res = cb(this.promiseResult);
+          if (res instanceof MyPromise) {
+            res.then(resolve, reject);
+          } else {
+            resolve(res);
+          }
+        } catch (err) {
+          reject(err);
+        }
+      };
+      if (this.promiseStatus === MyPromise.FULFILLED) {
+        resolvePromise(onFulfilled);
+        // onFulfilled(this.promiseResult);
+      } else if (this.promiseStatus === MyPromise.REJECTED) {
+        resolvePromise(onRejected);
+        // onRejected(this.promiseResult);
+      } else if (this.promiseStatus === MyPromise.PENDING) {
+        this.promiseFulfilledCallback.push(resolvePromise.bind(this, onFulfilled));
+        this.promiseRejectedCallback.push(resolvePromise.bind(this, onRejected));
+      };
+    });
+  };
+};
+
+var a = new MyPromise((resolve, reject) => {
+  console.log(1);
+  setTimeout(() => {
+    resolve(1);
+  }, 2000);
+}).then(res => {
+  console.log(res, '--第一个then');
+  return res + 1;
+}, err => err).then((res) => { console.log(res, '--第二个then'); }, (err) => { console.log(err, '--失败'); });
 
 // 针对 promise 分析
 // 1. 有 pending、fulfilled、rejected 三种状态，默认为 pending，且状态不可逆，只能从 pending -> resolve or pending -> reject
@@ -95,15 +96,16 @@ const PromiseRejected = 'rejected';
 // 3. 上一次的链式调用会影响后续的执行逻辑
 // 4. 需要考虑定时器、异步的情况，在 then 中 要往 successCb 和 failCb 数组中 push
 
+
 // 函数式实现
-function MyPromiseFn(executor) {
+function MyPromise(executor) {
   this.promiseResult = null;
-  this.promiseStatus = PromisePending;
+  this.promiseStatus = MyPromise.PENDING;
   this.promiseFulfilledCallbacks = [];
   this.promiseRejectedCallbacks = [];
   function resolve(result) {
-    if (this.promiseStatus !== PromisePending) return;
-    this.promiseStatus = PromiseFulfilled;
+    if (this.promiseStatus !== MyPromise.PENDING) return;
+    this.promiseStatus = MyPromise.FULFILLED;
     this.promiseResult = result;
     while (this.promiseFulfilledCallbacks.length) {
       const cb = this.promiseFulfilledCallbacks.shift();
@@ -111,8 +113,8 @@ function MyPromiseFn(executor) {
     }
   }
   function reject(reason) {
-    if (this.promiseStatus !== PromisePending) return;
-    this.promiseStatus = PromiseRejected;
+    if (this.promiseStatus !== MyPromise.PENDING) return;
+    this.promiseStatus = MyPromise.REJECTED;
     this.promiseResult = reason;
     while (this.promiseRejectedCallbacks.length) {
       const cb = this.promiseRejectedCallbacks.shift();
@@ -126,7 +128,7 @@ function MyPromiseFn(executor) {
     throw new Error(err);
   }
 };
-MyPromiseFn.prototype.then = function(thenResolve, thenReject) {
+MyPromise.prototype.then = function(thenResolve, thenReject) {
   thenResolve = typeof thenResolve === 'function'
     ? thenResolve
     : (val) => val;
@@ -135,7 +137,7 @@ MyPromiseFn.prototype.then = function(thenResolve, thenReject) {
     : (reason) => {
       throw new Error(reason);
     };
-  const thenPromise = new MyPromiseFn((resolve, reject) => {
+  const thenPromise = new MyPromise((resolve, reject) => {
     function resolvePromise(cb) {
       queueMicrotask(() => {
         try {
@@ -143,7 +145,7 @@ MyPromiseFn.prototype.then = function(thenResolve, thenReject) {
           if (result === thenPromise) {
             throw new Error('not return self');
           }
-          if (result instanceof MyPromiseFn) {
+          if (result instanceof MyPromise) {
             result.then(resolve, reject);
           } else {
             resolve(result);
@@ -154,13 +156,13 @@ MyPromiseFn.prototype.then = function(thenResolve, thenReject) {
         }
       });
     };
-    if (this.promiseStatus === PromiseFulfilled) {
+    if (this.promiseStatus === MyPromise.FULFILLED) {
       resolvePromise(thenResolve.bind(this));
       // thenResolve(this.promiseResult);
-    } else if (this.promiseStatus === PromiseRejected) {
+    } else if (this.promiseStatus === MyPromise.REJECTED) {
       // thenReject(this.promiseResult);
       resolvePromise(thenReject.bind(this));
-    } else if (this.promiseStatus === PromisePending) {
+    } else if (this.promiseStatus === MyPromise.PENDING) {
       // 异步、或者 空 promise
       this.promiseFulfilledCallbacks.push(resolvePromise.bind(this, thenResolve));
       this.promiseRejectedCallbacks.push(resolvePromise.bind(this, thenReject));
@@ -168,21 +170,20 @@ MyPromiseFn.prototype.then = function(thenResolve, thenReject) {
   });
   return thenPromise;
 };
-// var fn = new MyPromiseFn((resolve, reject) => {
+// var fn = new MyPromise((resolve, reject) => {
 //   setTimeout(() => {
 //     reject(1);
 //   }, 3000);
 // }).then((res) => { console.log(res, '--res'); }, (err) => { console.log(err, '--err'); });
 
 // 执行测试用例需要用到的代码
-MyPromiseFn.deferred = function() {
+MyPromise.deferred = function() {
   let defer = {};
-  defer.promise = new MyPromiseFn((resolve, reject) => {
+  defer.promise = new MyPromise((resolve, reject) => {
     defer.resolve = resolve;
     defer.reject = reject;
   });
   return defer;
 }
 
-module.exports = MyPromiseFn;
-// export default MyPromise;
+module.exports = MyPromise;
