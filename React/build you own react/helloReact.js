11a2ee67ce2d01d9ef5180405f8703713e90ba9b
FlyerWu <scofield90.email@gmail.com>
2024-03-06 18:20:32 +0800

    curry

--- React/build you own react/helloReact.js
+++ React/build you own react/helloReact.js
@@ -24,3 +24,25 @@ const domContainer = document.getElementById('root');
 const root = ReactDOM.createRoot(domContainer);
 
 root.render(React.createElement(LikeButton));
+
+function _add(...args) {
+  return args.reduce((prev, curr) => prev + curr, 0);
+}
+
+function curryFn(fn) {
+  let args = [];
+  return function cb(..._args) {
+    if (_args.length) {
+      args.push(..._args);
+      return cb;
+    } else {
+      const val = fn.apply(this, args);
+      args = [];
+      return val;
+    }
+  };
+};
+
+const add = curryFn(_add);
+
+add(1)(2)(3)
